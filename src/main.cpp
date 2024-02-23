5d34b2a33f236732d9e8ac0b6bc657316b4f0794
Joakkarl <joakkarl@playgroundsquad.com>
2024-02-23 16:40:36 +0100

    added some texture code and shader files(WIP)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <cstdio>
-
 #include "rendering/cWindow.h"
 #include "rendering/cRenderer.h"
 #include <cmath>
+#include <iostream>
+#include <filesystem>
+#include "rendering/Shader/Shader.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "rendering/stb_image.h"
 
 int main()
 {
@@ -12,18 +17,20 @@ int main()
 	window.create( 800, 600, "Catengine.exe" );
 	renderer.create( window );
 
-	float vertices[] = 
-	{
-		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+	Shader ourShader( "resources/shaders/4.1.texture.vs", "resources/shaders/4.1.texture.fs" );
+
+	float vertices[] = {
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 	unsigned int indices[] = 
 	{ 
-		0, 1, 2
+		0, 1, 3,
+		1, 2, 3
 	};
-
-	
 
 	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays( 1, &VAO );
@@ -38,16 +45,38 @@ int main()
 	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
 	glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices, GL_STATIC_DRAW );
 
-	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( float ), ( void* ) 0 );
+	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof( float ), ( void* ) 0 );
 	glEnableVertexAttribArray( 0 );
 
-	glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( float ), ( void* ) ( 3 * sizeof( float ) ) );
+	glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof( float ), ( void* ) ( 3 * sizeof( float ) ) );
 	glEnableVertexAttribArray( 1 );
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glVertexAttribPointer( 2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof( float ), ( void* ) ( 6 * sizeof( float ) ) );
+	glEnableVertexAttribArray( 2 );
 
-	 glBindVertexArray(0); 
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load(FileSystem::getPath("resources/textures/container.jpg").c_str(), &width, &height, &nrChannels, 0);
 
+	unsigned int texture;
+	glGenTextures( 1, &texture );
+	glBindTexture( GL_TEXTURE_2D, texture );
+
+
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+	if ( data )
+	{
+		glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data );
+		glGenerateMipmap( GL_TEXTURE_2D );
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free( data );
 
 	while ( !window.shouldClose() )
 	{
@@ -62,8 +91,9 @@ int main()
 		int vertexColorLocation = glGetUniformLocation( renderer.getDefaultShader(), "ourColor" );
 
 		glUseProgram( renderer.getDefaultShader() );
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray( VAO );
-		glDrawElements( GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0 );
+		glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
 		glBindVertexArray( 0 );
 
 
--- src/rendering/cRenderer.h
+++ src/rendering/cRenderer.h
@@ -23,20 +23,24 @@ private:
 		"#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;"
 		"layout (location = 1) in vec3 aColor;\n"
+		"layout (location = 2) in vec2 aTexCoord;\n"
 		"out vec3 ourColor;"
+		"out vec2 TexCoord;"
 		"void main()\n"
 		"{\n"
 		"   gl_Position = vec4(aPos, 1.0);\n"
 			"ourColor = aColor;"
+		"TexCoord = aTexCoord;"
 		"}\0";
 
 	const char* m_fragment_shader_default_source =
 		"#version 330 core\n"
-		"out vec4 FragColor;\n"
 		"in vec3 ourColor;\n"
+		"in vec2 TexCoord;\n"
+		"uniform sampler2D outTexture;\n"
 		"void main()\n"
 		"{\n"
-		"	FragColor = vec4(ourColor, 1.0);"
+		"	FragColor = vec4(ourTexture, TexCoord);"
 		"}\0";
 
 	unsigned int m_shader_default = 0;
